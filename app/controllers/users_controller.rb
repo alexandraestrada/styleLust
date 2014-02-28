d2450fe3830ec242a8f878628f63b81a850f8947
estrada88 <estrada.ae@gmail.com>
2014-02-27 20:59:54 -0800

    run 4 tests

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,19 @@
 class UsersController < ApplicationController
 	def index
-		@user = User.new
-		
+		@user = User.new		
+	end
+
+	def new
+		@user=User.new
+		redirect_to root_path
+
+	end
+
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def edit
+		@user = User.find(params[:id])
 	end
 end
--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -7,15 +7,39 @@ describe UsersController do
 			it 'returns http success' do				
 				User.should_receive(:new).and_return("blah")
 				get 'index'
-				expect(assigns(:user)).to eq "blah"
+				expect(assigns(:user)).to eq "blah"				
 				response.should be_success
 
 			end
+
 		end
-		# context 'new' do
-		# 	#it 'returns http success' do
-		# 	end
-		# end
+		context 'new' do
+			it 'returns http success' do
+				User.should_receive(:new).and_return("blah")
+				get 'new'
+				expect(assigns(:user)).to eq "blah"
+				expect(response).to redirect_to(root_path)
+			end
+		end
+
+		context 'show' do
+			it 'returns http success' do
+				User.should_receive(:find).with("5").and_return("qellieg")
+				get 'show', {id:5} 
+				expect(assigns(:user)).to eq "qellieg"
+				response.should be_success
+			end
+		end
+
+		context 'edit' do
+			it 'returns http success' do
+				User.should_receive(:find).with("5").and_return("waffles")
+				get 'edit', {id:5}
+				expect(assigns(:user)).to eq "waffles"
+				response.should be_success
+			end
+		end
+
 		# context 'edit' do
 		# 	it 'returns http success' do
 		# 	end
