2e4772e3014c9bf54921a9dab4c822d9507d165d
estrada88 <estrada.ae@gmail.com>
2014-02-28 17:48:32 -0800

    tests2

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,6 +1,6 @@
 class Item < ActiveRecord::Base
-  belongs_to :category
-  belongs_to :brand
-  has_many :likes
-  has_many :users, through: :likes, class_name: 'User'
+  # belongs_to :category
+  # belongs_to :brand
+  # has_many :likes
+  # has_many :users, through: :likes
 end
