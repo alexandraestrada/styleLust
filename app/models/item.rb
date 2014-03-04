22e7ec0c3d8ff617eed5d554f1f1b9fbb4431ce4
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-03-04 12:05:42 -0800

    temporarily comment out category and brand id validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,8 +5,8 @@ class Item < ActiveRecord::Base
   has_many :users, through: :likes
   validates :name, presence: {message: "name must be present"}
   validates :price, presence: {message: "price must be present"}
-  validates :category_id, presence: {message: "category_id must be present"}
-  validates :brand_id, presence: {message: "brand_id must be present"}
+  #validates :category_id, presence: {message: "category_id must be present"}
+  #validates :brand_id, presence: {message: "brand_id must be present"}
   validates :photo, presence: {message: "photo must be present"}
   validates :click_url, presence: {message: "click_url must be present"}
 
