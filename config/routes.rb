70b7e0c14ca3c71c4eb2c061dba3ae54d58a1093
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-03-04 19:26:00 -0800

    add items index styling and welcome page

--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ StyleLust::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'users#new'
+  root 'items#welcome'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
