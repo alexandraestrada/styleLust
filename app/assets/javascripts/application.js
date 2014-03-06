326e6ca257314d72f8d70d19896bcf32700e2e37
ElenaHoshizaki <elena.hoshizaki@gmail.com>
2014-03-05 18:55:43 -0800

    make everything responsive except for slide deck background image

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,7 +10,7 @@
 // Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require foundation
+// require foundation
 // require jquery
 //= require jquery_ujs
 //= require jquery.ui.all
